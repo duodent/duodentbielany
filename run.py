9d856d4483c4a76dc49cdcd7e473b3a398120dfe
duodent <katarzyna.zmuda@duodent.com.pl>
2025-02-15 12:20:13 +0100

    Working now

--- run.py
+++ run.py
@@ -8406,8 +8406,7 @@ for author_fb, mentions_list in dict_facebook_comments.items():
     if author_fb in list_lajki_posta_fb and author_fb in list_followers_fb and len(mentions_list) >= 3:
         uczestnicy_fb.append(author_fb)
 
-del session['zwyciezca_fb']
-del session['zwyciezca_ig']
+
 @app.route('/admin/losowanie')
 def drawing_of_competition_results():
     if not session.get('username'):
@@ -8419,6 +8418,9 @@ def drawing_of_competition_results():
     # Pobieramy zwycięzców z sesji
     zwyciezca_fb = session.get('zwyciezca_fb')
     zwyciezca_ig = session.get('zwyciezca_ig')
+    
+    del session['zwyciezca_fb']
+    del session['zwyciezca_ig']
 
     return render_template(
         "drawing_of_competition_results.html", 
